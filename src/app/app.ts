bd98f94cb561623bf34fb703e06a0a852cdac61d
Mohamed <mahmmaad96@gmail.com>
2026-06-06 14:18:57 +0300

    chore: scaffold frontend foundation

---
+++ src/app/app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: '<span class="font-sans">Quran Dashboard UI</span>',
+})
+export class App {
+}
